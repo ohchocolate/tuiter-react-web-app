b927ad8711b0eccd8263353ab31e51b28223e65d
ohchocolate <zhongxiaoyao1@gmail.com>
2022-10-15 19:48:39 -0400

    check list

--- public/tuiter/WhoToFollowList/WhoToFollowList.js
+++ public/tuiter/WhoToFollowList/WhoToFollowList.js
@@ -3,10 +3,10 @@ import who from './who.js';
 
 const WhoToFollowList = () => {
     return (`
-    <ul class="list-group d-none d-sm-none d-md-none d-lg-inline-block">
-        <li class="list-group-item fw-bolder">Who to follow</li>
-        ${who.map((item) => WhoToFollowListItem(item)).join('')}
-                
+            <ul class="list-group d-none d-sm-none d-md-none d-lg-inline-block">
+                <li class="list-group-item fw-bolder">Who to follow</li>
+                ${who.map((item) => WhoToFollowListItem(item)).join('')}
+            </ul>             
     `);
 };
 export default WhoToFollowList;
