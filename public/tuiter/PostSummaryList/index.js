3a6e9c6b54b535eb32a229962c7f01790dcbf550
ohchocolate <zhongxiaoyao1@gmail.com>
2022-10-15 19:37:16 -0400

    check js

--- public/tuiter/PostSummaryList/index.js
+++ public/tuiter/PostSummaryList/index.js
@@ -1,9 +1,9 @@
-import PostSummaryItem from "./PostSummaryItem";
-import posts from "./posts";
+import PostSummaryItem from "./PostSummaryItem.js";
+import posts from "./posts.js";
 
 const PostSummaryList = () => {
     return (`
-    ${posts.map((post) => PostSummaryItem(post)).join('')}
+        ${posts.map((post) => PostSummaryItem(post)).join('')}
     `);
 };
 export default PostSummaryList;
