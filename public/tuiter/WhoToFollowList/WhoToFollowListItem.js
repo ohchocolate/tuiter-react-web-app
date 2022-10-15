33e9b1605ad5a0e40f12fa82b8619188d55b1f43
ohchocolate <zhongxiaoyao1@gmail.com>
2022-10-15 19:47:22 -0400

    use who parameter

--- public/tuiter/WhoToFollowList/WhoToFollowListItem.js
+++ public/tuiter/WhoToFollowList/WhoToFollowListItem.js
@@ -1,16 +1,17 @@
+
 const WhoToFollowListItem = (who) => {
     return(`
     <li class="list-group-item">
         <div class="row d-flex justify-content-between">
             <div class="col-9">
                 <div class="row">
-                    <img src="https://pbs.twimg.com/profile_images/1410016948977422337/rKU8iR89_400x400.png" class="rounded-circle col-3">
+                    <img src="${who.avatarIcon}" class="rounded-circle col-3">
                     <div class="col">
                         <div class="fw-bolder">
-                            Java <i class="fas fa-check-circle"></i>
+                            ${who.userName} <i class="fas fa-check-circle"></i>
                         </div>
                         <div>
-                            @Java
+                            @${who.handle}
                         </div>
                     </div>
                 </div>
