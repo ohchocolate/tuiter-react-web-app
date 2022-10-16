e4dce2a856791e3fd5a0fab737dc0ba32120b539
ohchocolate <zhongxiaoyao1@gmail.com>
2022-10-16 15:12:50 -0400

    add driver func

---
+++ public/tuiter/PostList/PostList.js
@@ -0,0 +1,8 @@
+import PostItem from "./PostItem.js";
+import posts from "./posts.js";
+
+const PostList = () => {
+    return (`
+            ${posts.map((post) => PostItem(post)).join("")}
+    `)
+}
