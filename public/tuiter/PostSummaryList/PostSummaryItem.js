95de17d9f5729f6b0443e3267008a6073962a87c
ohchocolate <zhongxiaoyao1@gmail.com>
2022-10-16 13:12:40 -0400

    put all post summary item into a list-group-item

--- public/tuiter/PostSummaryList/PostSummaryItem.js
+++ public/tuiter/PostSummaryList/PostSummaryItem.js
@@ -1,19 +1,19 @@
 const PostSummaryItem = (post) => {
     return (`
     <div class="row wd-frame">
-        <div class="col-8">
-            <div class="text-secondary">
-                ${post.topic}
+        <div class="col-12 list-group">
+            <div class="list-group-item">
+                <div><img class="float-end rounded" src="${post.image}" height="100px"></div>
+                <div class="text-secondary">
+                    ${post.topic}
+                </div>
+                <div class="fw-bolder wd-tuiter-author">
+                    ${post.userName}<i class="fas fa-check-circle"></i> -${post.time}
+                </div>
+                <div class="fw-bolder">
+                    ${post.title}
+                </div>
             </div>
-            <div class="fw-bolder wd-tuiter-author">
-                ${post.userName}<i class="fas fa-check-circle"></i> -${post.time}
-            </div>
-            <div class="fw-bolder">
-                ${post.title}
-            </div>
-        </div>
-        <div class="col-4">
-            <img class="float-end rounded" src="${post.image}" height="100px">
         </div>
     </div>
   `);
