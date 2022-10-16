ad06944d95d63e359bf2767777fa97065dadb52a
ohchocolate <zhongxiaoyao1@gmail.com>
2022-10-16 16:09:57 -0400

    modify pic

--- public/tuiter/PostList/posts.js
+++ public/tuiter/PostList/posts.js
@@ -3,10 +3,10 @@ export default [
     {
         userName: 'Elon Musk',
         handle: 'elonmusk',
-        // icon: 'https://pbs.twimg.com/media/FAWRiSrUcAQgmYY.jpg',
+        icon: 'https://pbs.twimg.com/media/FAWRiSrUcAQgmYY.jpg',
         time: '23h',
         content: 'Amazing show about <a href="#">@Inspiration4x</a> mission!',
-        image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Finspiration4.com%2F&psig=AOvVaw3uZzFUGX5y_7sQYIrpf6aJ&ust=1665973343260000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCICP4r_Y4_oCFQAAAAAdAAAAABAD',
+        image: 'https://i.cbc.ca/1.6176796.1631726112!/fileImage/httpImage/image.JPG_gen/derivatives/original_780/space-exploration-spacex.JPG',
         title: 'Countdown: Inspiration4 Mission to Space | Netflix Official Site',
         summary:
             'From training to launch to landing, this all-access docuseries rides along with the ' +
@@ -24,7 +24,7 @@ export default [
         content:
             'Grimes seen reading Karl Marx following split with world\'s richest man Elon Musk' +
             ' <a href="#">trib.al/nx2Gfaq</a>',
-        image: 'https://media.vanityfair.com/photos/603e71d829e5b22a654c9679/master/pass/1133728517',
+        image: 'https://bioprepwatch.com/wp-content/uploads/2022/03/Elon-Musk-Grimes-Definitely-They-have-secretly-become-parents.jpg',
         comment_count: '965',
         retweet_count: '2.4k',
         like_count: '4k',
