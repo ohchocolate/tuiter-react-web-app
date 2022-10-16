4504963a6021c2fdf5c9405198c515378c48bb2f
ohchocolate <zhongxiaoyao1@gmail.com>
2022-10-15 22:29:24 -0400

    add pic link

--- public/tuiter/PostList/posts.js
+++ public/tuiter/PostList/posts.js
@@ -2,10 +2,10 @@ export default [
     {
         userName: 'Elon Musk',
         handle: 'elonmusk',
-        icon: '../../images/elon_musk_icon.jpeg',
+        icon: 'https://pbs.twimg.com/media/FAWRiSrUcAQgmYY.jpg',
         time: '23h',
         content: 'Amazing show about <a href="#">@Inspiration4x</a> mission!',
-        image: '../../images/inspiration4.jpeg',
+        image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Finspiration4.com%2F&psig=AOvVaw3uZzFUGX5y_7sQYIrpf6aJ&ust=1665973343260000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCICP4r_Y4_oCFQAAAAAdAAAAABAD',
         title: 'Countdown: Inspiration4 Mission to Space | Netflix Official Site',
         summary:
             'From training to launch to landing, this all-access docuseries rides along with the ' +
@@ -18,11 +18,12 @@ export default [
     {
         userName: 'New York Post',
         handle: 'nypost',
-        icon: '../../images/new_york_post_icon.png',
+        icon: 'https://w7.pngwing.com/pngs/691/895/png-transparent-new-york-post-manhattan-new-york-daily-news-newspaper-journalist-taiyaki-text-logo-united-states-thumbnail.png',
         time: '23h',
         content:
-            'Grimes seen reading Karl Marx following split with world\'s richest man Elon Musk <a href="#">trib.al/nx2Gfaq</a>',
-        image: '../../images/new_york_post_summary.jpeg',
+            'Grimes seen reading Karl Marx following split with world\'s richest man Elon Musk' +
+            ' <a href="#">trib.al/nx2Gfaq</a>',
+        image: 'https://media.vanityfair.com/photos/603e71d829e5b22a654c9679/master/pass/1133728517',
         comment_count: '965',
         retweet_count: '2.4k',
         like_count: '4k',
