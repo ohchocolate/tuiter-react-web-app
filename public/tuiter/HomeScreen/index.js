41e98c77c2aad764dde1340a16c2bcf30a7d39b0
ohchocolate <zhongxiaoyao1@gmail.com>
2022-10-16 11:54:36 -0400

    add file

---
+++ public/tuiter/HomeScreen/index.js
@@ -0,0 +1,22 @@
+/* eslint-env jquery */
+import NavigationSidebar from "../NavigationSidebar/index.js";
+import PostSummaryList from "../PostSummaryList.js";
+
+
+function exploreComponent() {
+    $('#wd-explore').append(`
+          <div class="row m-auto">
+                    <div class="col-2 col-md-2 col-lg-1 col-xl-2">
+                        ${NavigationSidebar('Explore')}
+                    </div>
+                    <div class="col-10 col-md-10 col-lg-7 col-xl-6">
+                        ${PostItem()}
+                    </div>
+                    <div class="col-lg-4 col-xl-4 d-none d-lg-block">
+                        ${PostSummaryList()}
+                    </div>
+          </div>
+   `);
+}
+
+$(exploreComponent);
