e6f639d5adcabfcf13872fb6ecf9d983781a84c4
ohchocolate <zhongxiaoyao1@gmail.com>
2022-10-18 15:32:12 -0400

    a6 lab

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Labs from "./labs";
+import HelloWorld from "./labs/a6/hello-world";
+import Tuiter from "./tuiter";
+import {BrowserRouter} from "react-router-dom";
+import {Routes} from "react-router";
+import {Route} from "react-router";
 
 //the entry point of the React.js application
 function App() {
     return (
-        <h1>Hello World!</h1>
+        <BrowserRouter>
+            <div className="container">
+                <Routes>
+                    <Route index
+                           element={<Labs/>}/>
+                    <Route path="/hello"
+                           element={<HelloWorld/>}/>
+                    <Route path="/tuiter"
+                           element={<Tuiter/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
