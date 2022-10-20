147b5832976ba79623aa67d4223ae901125e0777
ohchocolate <zhongxiaoyao1@gmail.com>
2022-10-19 23:03:07 -0400

    a6

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,11 +1,23 @@
-import Nav from "../nav";
+import React from "react";
+import ExploreComponent from "./explore";
+import NavigationSidebar
+    from "./navigation-sidebar";
+import WhoToFollowListItem from "./who-to-follow-list/who-to-follow-list-item";
 
 function Tuiter() {
-    return(
-        <div>
-            <Nav/>
-            <h1>Tuiter</h1>
+    return (
+        <div className="row m-auto">
+            <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+                <NavigationSidebar active="Explore"/>
+            </div>
+            <div className="col-10 col-md-10 col-lg-7 col-xl-6">
+                <ExploreComponent/>
+            </div>
+            <div className="col-10 col-md-10 col-lg-7 col-xl-6">
+                <WhoToFollowListItem/>
+            </div>
         </div>
     )
 }
+
 export default Tuiter
