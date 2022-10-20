dd075561ac5927febf2aae1ba9dafee3c8033bbc
ohchocolate <zhongxiaoyao1@gmail.com>
2022-10-19 23:21:39 -0400

    a6

---
+++ src/tuiter/post-summary-list/post-summary-item.js
@@ -0,0 +1,35 @@
+import React from "react";
+const PostSummaryItem = (
+    {
+        post = {
+            "topic": "Web Development",
+            "userName": "ReactJS",
+            "time": "2h",
+            "title": "React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs",
+            "image": "../../images/react.png"
+        }
+    }
+) => {
+    return(
+        <div className="row wd-frame">
+            <div className="col-12 list-group">
+                <div className="list-group-item">
+                    <div>
+                        <img className="float-end rounded" src={`/images/${post.image}`} height="100px">
+                        </img>
+                    </div>
+                    <div className="text-secondary">
+                        {post.topic}
+                    </div>
+                    <div className="fw-bolder wd-author">
+                        {post.userName} -{post.time}
+                    </div>
+                    <div className="fw-bolder">
+                        {post.title}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default PostSummaryItem;
