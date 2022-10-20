5ef44b2a8bcebd758ac7073ba8645b2b45083747
ohchocolate <zhongxiaoyao1@gmail.com>
2022-10-19 23:55:51 -0400

    adjust framework

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,23 +1,23 @@
 import React from "react";
-import ExploreComponent from "./explore";
+import ExploreComponent from "./explore/index.js";
 import NavigationSidebar
-    from "./navigation-sidebar";
-import WhoToFollowListItem from "./who-to-follow-list/who-to-follow-list-item";
+    from "./navigation-sidebar/index.js";
+import WhoToFollowList from "./who-to-follow-list/index.js";
 
 function Tuiter() {
     return (
-        <div className="row m-auto">
-            <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-                <NavigationSidebar active="Explore"/>
+        <div className='row mt-2'>
+            <div className='col-2 col-md-2 col-lg-1 col-xl-2'>
+                <NavigationSidebar active='Explore' />
             </div>
-            <div className="col-10 col-md-10 col-lg-7 col-xl-6">
-                <ExploreComponent/>
+            <div className='col-10 col-md-10 col-lg-7 col-xl-6'>
+                <ExploreComponent />
             </div>
-            <div className="col-10 col-md-10 col-lg-7 col-xl-6">
-                <WhoToFollowListItem/>
+            <div className='col-lg-4 col-xl-4 d-none d-lg-block'>
+                <WhoToFollowList />
             </div>
         </div>
     );
-};
+}
 
 export default Tuiter;
