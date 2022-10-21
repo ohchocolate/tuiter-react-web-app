7ae307769d0462f0972f3fc5ebb5b8c205d73762
ohchocolate <zhongxiaoyao1@gmail.com>
2022-10-21 12:23:27 -0400

    a6

---
+++ src/tuiter/who-to-follow-list/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Who from './who.json';
+import WhoToFollowListItem from "./who-to-follow-list-item";
+
+const WhoToFollowList = () => {
+    return(
+        <div className="list-group d-none d-sm-none d-md-none d-lg-inline-block">
+            <li className="list-group-item fw-bolder">
+                <h3>Who to follow</h3>
+            </li>
+
+            {
+                Who.map((item) => (<WhoToFollowListItem who={item}/>))
+            }
+
+        </div>
+    );
+};
+export default WhoToFollowList;
