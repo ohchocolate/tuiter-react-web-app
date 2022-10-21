f6365d758030edecb2ca99011aa51ac50863678f
ohchocolate <zhongxiaoyao1@gmail.com>
2022-10-21 12:36:46 -0400

    a6

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -8,7 +8,7 @@ function Tuiter() {
     return (
         <div className='row mt-2'>
             <div className='col-2 col-md-2 col-lg-1 col-xl-2'>
-                <NavigationSidebar active='Explore'/>
+                <NavigationSidebar active='explore'/>
             </div>
             <div className='col-10 col-md-10 col-lg-7 col-xl-6'>
                 <ExploreComponent />
