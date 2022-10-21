09317e1df64ae68c1c11aad04490ff1f508d07b4
ohchocolate <zhongxiaoyao1@gmail.com>
2022-10-21 12:23:51 -0400

    a6

---
+++ src/tuiter/who-to-follow-list/who-to-follow-list-item.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const WhoToFollowListItem = (
+    {
+        who = {
+            avatarIcon: "https://pbs.twimg.com/profile_images/1410016948977422337/rKU8iR89_400x400.png",
+            userName: "Java",
+            handle: "java" }
+    }
+) => {
+    return(
+        <div>
+        <li className="list-group-item">
+            <div className="row d-flex justify-content-between">
+                <div className="col-9">
+                    <div className="row">
+                        <img src={who.avatarIcon} className="rounded-circle col-3"/>
+                            <div className="col">
+                                <div className="fw-bolder">
+                                    {who.userName}
+                                </div>
+                                <div>
+                                    @{who.handle}
+                                </div>
+                            </div>
+                    </div>
+                </div>
+                <div className="col-3 m-auto">
+                    <button className="btn btn-primary btn-block rounded-pill m-auto">
+                        Follow
+                    </button>
+                </div>
+            </div>
+        </li>
+        </div>
+    );
+};
+export default WhoToFollowListItem;
