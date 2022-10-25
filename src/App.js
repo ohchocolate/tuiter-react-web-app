95bdd2bbb770d58d329de4cb929c294ac78fc432
ohchocolate <zhongxiaoyao1@gmail.com>
2022-10-25 18:06:02 -0400

    a7 lab fix color

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ function App() {
                            element={<HelloWorld/>}/>
                     <Route path="/tuiter/*"
                            element={<Tuiter/>}/>
+                    <Route path="/*"
+                           element={<Labs/>}/>
                 </Routes>
             </div>
         </BrowserRouter>
