e625004a7a65cd73680b1b1d7d10fc907d082a30
ohchocolate <zhongxiaoyao1@gmail.com>
2022-10-25 17:29:20 -0400

    a7 lab

---
+++ src/labs/a7/redux-examples/reducers/hello.js
@@ -0,0 +1,6 @@
+// learn how data can be shared across multiple components,
+// and how each can interact with the data like reading and writing to it.
+
+const hello = () => ({message: 'Hello World'});
+
+export default hello;
