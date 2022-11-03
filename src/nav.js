5cb3cc525fde42159de5329930d2689650385eaf
ohchocolate <zhongxiaoyao1@gmail.com>
2022-11-03 14:25:59 -0400

    change path using react useLocation

--- src/nav.js
+++ src/nav.js
@@ -1,14 +1,14 @@
 import {Link} from "react-router-dom";
+import {useLocation} from "react-router";
 
-function Nav() {
+export default () => {
+    const {pathname} = useLocation()
+    console.log(pathname)
     return (
-        <div>
-            <Link to="/">Labs</Link> |
-            <Link to="/">Assignment 6</Link> |
-            <Link to="/a7">Assignment 7</Link> |
-            <Link to="/tuiter">Tuiter</Link>
+        <div className="nav nav-pills">
+            <Link className={`nav-link ${pathname.indexOf('tuiter') !== -1 ? 'active': ''}`} to="/tuiter">Tuiter</Link>
+            <Link className={`nav-link ${pathname === '/' ? 'active': ''}`} to="/">Assignment 6</Link>
+            <Link className={`nav-link ${pathname.indexOf('a7') !== -1 ? 'active': ''}`} to="/a7">Assignment 7</Link>
         </div>
-    );
+    )
 }
-
-export default Nav;
