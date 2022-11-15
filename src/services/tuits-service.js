f3c910ab0280dc7a5cd57f2dd06adc8b5c77e8c5
ohchocolate <zhongxiaoyao1@gmail.com>
2022-11-14 22:27:11 -0500

    fix API loading problem

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-// const TUITS_API = 'http://localhost:4000/api/tuits';
-// const TUITS_API = 'https://tuiter-node-server-app-22fall.herokuapp.com/api/tuits';
-const API_BASE = process.env.REACT_APP_API_BASE;
+
+const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:4000/api';
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
@@ -10,10 +9,10 @@ export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
     return response.data;
 }
+
 export const findTuits  = async ()     => {
     const response = await axios.get(TUITS_API);
-    const tuits = response.data;
-    return tuits;
+    return response.data;
 }
 
 export const deleteTuit = async (tid) => {
