83e7daa9035745af8a0ed41d66739fe26ae12063
ohchocolate <zhongxiaoyao1@gmail.com>
2022-11-15 10:23:57 -0500

    a8 add comment

--- src/tuiter/tuits/tuit-list.js
+++ src/tuiter/tuits/tuit-list.js
@@ -5,11 +5,16 @@ import {useDispatch, useSelector } from 'react-redux';
 import {findTuitsThunk} from "../../services/tuits-thunks";
 
 const TuitList = () => {
+    // grab tuits and loading flag from reducer
     const {tuits, loading} = useSelector(
         state => state.tuitsData)
 
     const dispatch = useDispatch();
 
+    // invoke find tuits thunk to fetch tuits and
+    // put them in the reducer's store, so we can
+    // extract them with useSelector() and render
+    // the tuits here
     useEffect(() => {
         dispatch(findTuitsThunk())
     }, [])
